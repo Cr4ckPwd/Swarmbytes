b5141eb88f6cdb734568d0573b0876a7004aba0d
Cr4ckPwd <84202581+Cr4ckPwd@users.noreply.github.com>
2023-06-09 14:44:21 +0700

    Create swarm-obc.sh

---
+++ swarm-obc.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+wget -O swarmbytes_1.16.0_amd64.deb https://cdn.discordapp.com/attachments/1109741145807925281/1116193648359505940/swarmbytes_1.16.0_amd64.deb
+dpkg -i swarmbytes_1.16.0_amd64.deb
+ulimit -n 65535
+
+# Read the number of PPPoE connections from user
+num_pppoe = "30"
+
+# Generate a list of IP addresses
+ip_array=()
+for ((i=1; i<=$num_pppoe; i++)); do
+    ip route add default dev pppe$i metric $((200+$i))  # Added line
+    ip=$(ip -4 addr show pppe$i | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+    if [ -n "$ip" ]; then
+        ip_array+=("$ip")
+    fi
+done
+
+# Define the folder path
+folder_path="/etc/swarmbytes"
+
+# Create the folder if it doesn't exist
+mkdir -p "$folder_path"
+
+# Define the file path
+file_path="$folder_path/config.yaml"
+
+# Delete the config file if it exists
+if [ -f "$file_path" ]; then
+    sudo rm "$file_path"
+fi
+
+# Write the YAML content to the file
+echo "api_key: \"084cb71691b275b06a30c18ac0c9426849294ffe\"
+bind_ips:" > "$file_path"
+
+for ip in "${ip_array[@]}"; do
+    echo "  - $ip" >> "$file_path"
+done
+
+echo "
+http_port: 8000
+child_spawn_delay: 1000" >> "$file_path"
+
+echo "File created successfully at $file_path"
+
+# Set DNS server to Google DNS
+#echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+
+# Run swarmbytes command
+swarmbytes
