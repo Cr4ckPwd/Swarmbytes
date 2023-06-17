5b3664c2647d2847648441a6e7d583790b238467
Cr4ckPwd <84202581+Cr4ckPwd@users.noreply.github.com>
2023-06-17 15:02:23 +0700

    Create Final.sh

---
+++ Final.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# Update package lists and install required packages
+sudo apt update && sudo apt install pppoeconf iproute2 wget -y
+
+# Display detected primary interfaces
+echo "Detected Primary Interfaces: "
+ls /sys/class/net | grep -v lo
+
+# Read the physical interface from user
+read -p "Enter the physical interface name: " physical_interface
+
+# Read API key from user
+read -p "Enter your Swarmbytes API key: " api_key
+
+# Set physical interface up
+sudo ip link set "$physical_interface" up
+
+# Read the number of PPPoE connections from user
+read -p "Enter the number of PPPoE connections you want to create: " num_pppoe
+
+# Read username from user
+read -p "Enter your username: " username
+
+# Read password from user (password won't be displayed)
+read -s -p "Enter your password: " password
+echo
+
+# Set credentials in pap-secrets
+sudo sh -c "echo '\"$username\" * \"$password\"' > /etc/ppp/pap-secrets"
+
+# Set file permissions for pap-secrets
+sudo chmod 600 /etc/ppp/pap-secrets
+
+# Create macvlan interfaces, set them up, and start PPPoE connections
+for i in $(seq 1 $num_pppoe); do
+    sudo ip link add link "$physical_interface" macvlan$i type macvlan mode bridge
+    sudo ip link set macvlan$i up
+
+    # Check if configuration file exists and remove it
+    if [ -f "/etc/ppp/peers/macvlan$i" ]; then
+        sudo rm "/etc/ppp/peers/macvlan$i"
+    fi
+
+    # Create PPPoE configuration file and set credentials
+    sudo sh -c "echo '
+noipdefault
+defaultroute    
+hide-password
+lcp-echo-interval 20
+lcp-echo-failure 3
+connect /bin/true
+noauth
+persist
+mtu 1500
+ifname pppoe$i
+noaccomp
+default-asyncmap
+plugin rp-pppoe.so macvlan$i
+user \"$username\"
++ipv6
+updetach
+maxfail 10
+' > /etc/ppp/peers/macvlan$i"
+
+    # Start PPPoE connection
+    sudo pon macvlan$i
+
+    # Add default route for PPPoE connection
+    sudo ip route add default dev pppoe$i metric $((100+$i))
+done
+
+# Generate a list of IP addresses
+ip_array=()
+for ((i=1; i<=$num_pppoe; i++)); do
+    ip=$(ip -4 addr show pppoe$i | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+    if [ -n "$ip" ]; then
+        ip_array+=("$ip")
+    fi
+done
+
+# Define the folder path
+folder_path="/etc/swarmbytes"
+
+# Define the file path
+file_path="$folder_path/config.yaml"
+
+# Check if config file exists and remove it
+if [ -f "$file_path" ]; then
+    sudo rm "$file_path"
+fi
+
+# Create the folder if it doesn't exist
+mkdir -p "$folder_path"
+
+# Write the YAML content to the file
+echo "api_key: \"$api_key\"
+bind_ips:" > "$file_path"
+
+for ip in "${ip_array[@]}"; do
+    echo "  - $ip" >> "$file_path"
+done
+
+echo "
+http_port: 8000
+child_spawn_delay: 1000" >> "$file_path"
+
+echo "File created successfully at $file_path"
+
+# Download Swarmbytes package
+wget -O swarmbytes_1.16.0_amd64.deb https://cdn.discordapp.com/attachments/1109741145807925281/1116193648359505940/swarmbytes_1.16.0_amd64.deb
+
+# Install Swarmbytes package
+dpkg -i swarmbytes_1.16.0_amd64.deb
+
+# Set ulimit
+ulimit -n 65535
+ufw allow from any to any
+
+# Set DNS server to Google DNS
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+
+# Run Swarmbytes command
+
+
+# Remove Swarmbytes package
+rm swarmbytes_1.16.0_amd64.deb
+# Cleanup
+rm -rf ./bash*
+rm pppoe.sh
